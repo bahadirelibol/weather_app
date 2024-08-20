ea3f7a2877be6f1a17705551cd9bb22fbf2c1434
Y. Bahadır Elibol <ybahadirelibol@gmail.com>
2024-08-20 18:24:20 +0300

    revision

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import customtkinter as ctk
+
 import tkinter as tk
+
 import requests
+
 
 def get_weather():
     city = city_entry.get().strip()
-    url =#enter a url
+    url = "#enter a url"
     headers = {
         "X-RapidAPI-Key": "#enter a key",
         "X-RapidAPI-Host": "#enter a host"
@@ -17,37 +21,52 @@ def get_weather():
         weather_desc = weatherdata["current"]["condition"]["text"]
         temperature = weatherdata["current"]["temp_c"]
         humidity = weatherdata["current"]["humidity"]
-        result_label.config(text=f"City: {location}\nWeather: {weather_desc}\nTemperature: {temperature}°C\nHumidity: {humidity}%")
+        result_label.configure(
+            text=f"City: {location}\nWeather: {weather_desc}\nTemperature: {temperature}°C\nHumidity: {humidity}%")
     except requests.exceptions.RequestException as e:
-        result_label.config(text="Error: There was a problem sending the request!")
+        result_label.configure(text="Error: There was a problem sending the request!")
     except KeyError:
-        result_label.config(text="No city found or weather data retrieved!")
+        result_label.configure(text="No city found or weather data retrieved!")
 
-#window
-window = tk.Tk()
-window.title("Weather App")
-window.minsize(width=650, height=700)
+
+def close_app():
+    app.destroy()
+
+
+ctk.set_appearance_mode("light")  # light mode
+ctk.set_default_color_theme("blue")  # Blue theme
+
+app = ctk.CTk()
+app.geometry("650x550")
+app.title("Weather App")
+app.config(background="#f2f4f4")
 
 #photolabel
 weather_photo = tk.PhotoImage(file="wa.png")
-photo_label = tk.Label(image=weather_photo)
+photo_label = tk.Label(image=weather_photo, background="#f2f4f4")
 photo_label.pack()
 
 #label
-city_label = tk.Label(window, text="City:", font=("Times", 30), fg="#FF6A6A")
+city_label = ctk.CTkLabel(app, text="City:", font=("Lucida Handwriting", 30), fg_color="#f2f4f4")
 city_label.pack(pady=5)
 
 #entry
-city_entry = tk.Entry(bg="#FFFAF0", font=10)
+city_entry = ctk.CTkEntry(app, width=200, font=("Helvetica", 15))
 city_entry.pack(pady=5)
 city_entry.focus()
 
 #button
-query_button = tk.Button(window, font=7, text="Search", command=get_weather, fg="#8B3A3A")
+query_button = ctk.CTkButton(app, text="Search", command=get_weather, font=("Helvetica", 12), fg_color="#5dade2",
+                             hover_color="#85c1e9")
 query_button.pack(pady=5)
 
 #label
-result_label = tk.Label(window, text="", font=('Helvetica', 20, 'bold'), fg="light blue")
+result_label = ctk.CTkLabel(app, text="", font=('Helvetica', 20, 'bold'), fg_color="#f2f4f4")
 result_label.pack(pady=10)
 
-window.mainloop()
+#exitbutton
+exit_btn = ctk.CTkButton(app, text="Exit", command=close_app, font=("Helvetica", 12), fg_color="#d5d8dc",
+                         hover_color="#c0392b")
+exit_btn.pack(padx=10, pady=10, side='right', anchor='s')
+
+app.mainloop()
